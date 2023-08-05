3b959ae9f605e3ba926e9451489ce263b438ac28
baeggoose <bsj154@gmail.com>
2023-08-05 15:29:49 +0900

    Nav components 생성

--- my-diary/src/App.js
+++ my-diary/src/App.js
@@ -7,14 +7,14 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/login" element={<Login/>}></Route>
-        <Route path="/signup" element={<Signup/>}></Route>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+        </Routes>
       </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
