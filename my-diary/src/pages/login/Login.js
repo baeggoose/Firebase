ab26783611bc08e7a5199c3d4af548bb0193e006
baeggoose <bsj154@gmail.com>
2023-08-07 09:39:27 +0900

    useState Hook을 이용한 Login Form

--- my-diary/src/pages/login/Login.js
+++ my-diary/src/pages/login/Login.js
@@ -1,9 +1,48 @@
-import styles from './Login.module.css'
+import { useState } from "react";
+import styles from "./Login.module.css";
 
-export default function Login(){
-  return(
-    <div>
-      Login
-    </div>
-  )
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handledata = (event) => {
+    if (event.target.type === "email") {
+      setEmail(event.target.value);
+    } else if (event.target.type === "password") {
+      setPassword(event.target.value);
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(email, password);
+  };
+
+  return (
+    <form className={styles.login_form} onSubmit={handleSubmit}>
+      <fieldset>
+        <legend>Login</legend>
+        <label htmlFor="myEmail">email : </label>
+        <input
+          type="email"
+          id="myEmail"
+          required
+          value={email}
+          onChange={handledata}
+        />
+
+        <label htmlFor="myPassword">password : </label>
+        <input
+          type="password"
+          id="myPassword"
+          required
+          value={password}
+          onChange={handledata}
+        />
+
+        <button type="submit" className={styles.btn}>
+          Login
+        </button>
+      </fieldset>
+    </form>
+  );
 }
