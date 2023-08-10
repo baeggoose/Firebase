3b8a84b9debef7abd8004bbd95e08f5928b1ccbe
baeggoose <bsj154@gmail.com>
2023-08-10 10:50:57 +0900

    Navigate 컴포넌트를 사용해 로그인 여부에 따른 페이지 렌더링 되도록 App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
 import { useAuthContext } from "./hooks/useAuthContext";
 import Home from "./pages/home/Home";
@@ -6,7 +6,7 @@ import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 
 function App() {
-  const { isAuthReady } = useAuthContext();
+  const { isAuthReady, user } = useAuthContext();
 
   return (
     <div className="App">
@@ -14,9 +14,21 @@ function App() {
         <BrowserRouter>
           <Nav />
           <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/signup" element={<Signup />}></Route>
+            <Route
+              path="/"
+              element={
+                // 로그인이 되어있을시, 로그인화면이나 회원가입 화면으로 돌아가지 못함
+                user ? <Home /> : <Navigate replace={true} to="/login" />
+              }
+            ></Route>
+            <Route
+              path="/login"
+              element={!user ? <Login /> : <Navigate replace={true} to="/" />}
+            ></Route>
+            <Route
+              path="/signup"
+              element={!user ? <Signup /> : <Navigate replace={true} to="/" />}
+            ></Route>
           </Routes>
         </BrowserRouter>
       ) : (
