489f51734cc4eff5b87036aacbcc7234433955ec
baeggoose <bsj154@gmail.com>
2023-08-10 13:51:58 +0900

    home 페이지에 사용될 DiaryForm.js컴포넌트 생성

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,13 @@
-import styles from './Home.module.css'
+import DiaryForm from "./DiaryForm";
+import styles from "./Home.module.css";
 
-export default function Home(){
-  return(
-    <div>
-      Home
-    </div>
-  )
+export default function Home() {
+  return (
+    <main className={styles.cont}>
+      <aside className={styles.side_menu}>
+        <DiaryForm></DiaryForm>
+      </aside>
+      <ul className={styles.content_list}>diary list</ul>
+    </main>
+  );
 }
