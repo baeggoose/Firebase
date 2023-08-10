8594dce8edb0ea984d2dc52eeed5a39ca5ee166e
baeggoose <bsj154@gmail.com>
2023-08-10 18:22:10 +0900

    DiaryForm.js 데이터 관리

--- src/pages/home/DiaryForm.js
+++ src/pages/home/DiaryForm.js
@@ -1,14 +1,43 @@
+import { useState } from "react";
+
 export default function DiaryForm() {
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+
+  const handledata = (event) => {
+    if (event.target.id === "tit") {
+      setTitle(event.target.value);
+    } else if (event.target.id === "txt") {
+      setText(event.target.value);
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(title, text);
+  };
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <fieldset>
           <legend>일기 쓰기</legend>
           <label htmlFor="tit">일기 제목 : </label>
-          <input id="tit" type="text" required />
+          <input
+            id="tit"
+            type="text"
+            required
+            onChange={handledata}
+            value={title}
+          />
 
-          <label htmlFor="tit">일기 내용 : </label>
-          <textarea id="tit" type="text" required></textarea>
+          <label htmlFor="txt">일기 내용 : </label>
+          <textarea
+            id="txt"
+            type="text"
+            required
+            onChange={handledata}
+            value={text}
+          ></textarea>
 
           <button type="submit">저장하기</button>
         </fieldset>
